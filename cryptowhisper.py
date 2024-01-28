38d4e10146f1b7658fa0dd33e54b025e61558db0
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-28 17:52:55 +0800

    details about the project

---
+++ cryptowhisper.py
@@ -0,0 +1,5 @@
+# PROJECT: CRYPTOWHISPER: Secret Messaging
+
+# Topic: Cryptography
+# About: This project is all about encrypting and descrypting of hidden/secret messages.
+# Functionality:  The users can send personal messages and information with privacy. 
